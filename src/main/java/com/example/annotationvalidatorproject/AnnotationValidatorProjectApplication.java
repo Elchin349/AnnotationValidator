2a55c55c5daf8c456915b133329adf454092287d
elchin349 <elchin.akbarov349@gmail.com>
2022-03-18 14:26:11 +0400

    refactor

--- src/main/java/com/example/annotationvalidatorproject/AnnotationValidatorProjectApplication.java
+++ src/main/java/com/example/annotationvalidatorproject/AnnotationValidatorProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnnotationValidatorProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AnnotationValidatorProjectApplication.class, args);
     }
-
 }
